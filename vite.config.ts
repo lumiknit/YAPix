1afcd7130ed615b14efb41541cb7a7062a61441c
lumiknit <aasr4r4@gmail.com>
2024-06-19 12:43:21 +0900

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,17 @@ export default defineConfig({
 	resolve: {
 		alias: [{ find: "@", replacement: "/src" }],
 	},
+	build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+					const names = id.split("node_modules/");
+					if (names.length > 1) {
+						const name = names.pop()!.split("/")[0];
+						return `vendor-${name}`;
+					}
+        },
+      },
+    },
+  },
 });
